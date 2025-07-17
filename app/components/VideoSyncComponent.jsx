b759bdcecd82d8e4eaeb01d759dce1803cd9b8c5
Belghieti <48102011+Belghieti@users.noreply.github.com>
2025-07-17 22:32:20 +0200

    Update VideoSyncComponent.jsx

--- app/components/VideoSyncComponent.jsx
+++ app/components/VideoSyncComponent.jsx
@@ -86,7 +86,7 @@ export default function VideoSyncComponent({ boxId }) {
     const token = localStorage.getItem("token");
     if (!boxInfo?.id || !token) return;
 
-    const socket = new SockJS(`${baseUrl}/ws`);
+    const socket = new SockJS(`${baseUrl.replace(/^https/, "http")}/ws`)
     const client = new Client({
       webSocketFactory: () => socket,
       onConnect: () => {
