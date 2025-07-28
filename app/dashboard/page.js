37b09626cedb5f2413c670f93addec886e40ce55
Belghieti <mohamedbelghieti1@gmail.com>
2025-07-28 04:26:12 +0200

    v1

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -339,7 +339,7 @@ export default function Dashboard() {
             </div>
 
             {/* Section des composants Guide et À propos */}
-            <div className="mt-12 space-y-8">
+            <div className="mt-12 space-y-12">
               {/* Guide Component */}
               <GuideComponent onMovieAdded={handleMovieAdded} />
 
